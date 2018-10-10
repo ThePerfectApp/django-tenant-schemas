d4aed53478f1fb60aef8c5552e2ecbcd6fb3248a
Peder Toftegaard Olsen <pdr@theperfectapp.com>
2018-10-10 12:44:56 +0200

    Make nose ignore methods.

--- tenant_schemas/test/cases.py
+++ tenant_schemas/test/cases.py
@@ -9,20 +9,20 @@ ALLOWED_TEST_DOMAIN = '.test.com'
 
 class TenantTestCase(TestCase):
     @classmethod
-    def add_allowed_test_domain(cls):
+    def _add_allowed_test_domain(cls):
         # ALLOWED_HOSTS is a special setting of Django setup_test_environment so we can't modify it with helpers
         if ALLOWED_TEST_DOMAIN not in settings.ALLOWED_HOSTS:
             settings.ALLOWED_HOSTS += [ALLOWED_TEST_DOMAIN]
 
     @classmethod
-    def remove_allowed_test_domain(cls):
+    def _remove_allowed_test_domain(cls):
         if ALLOWED_TEST_DOMAIN in settings.ALLOWED_HOSTS:
             settings.ALLOWED_HOSTS.remove(ALLOWED_TEST_DOMAIN)
 
     @classmethod
     def setUpClass(cls):
         cls.sync_shared()
-        cls.add_allowed_test_domain()
+        cls._add_allowed_test_domain()
         tenant_domain = 'tenant.test.com'
         cls.tenant = get_tenant_model()(domain_url=tenant_domain, schema_name='test')
         cls.tenant.save(verbosity=0)  # todo: is there any way to get the verbosity from the test command here?
@@ -34,7 +34,7 @@ class TenantTestCase(TestCase):
         connection.set_schema_to_public()
         cls.tenant.delete()
 
-        cls.remove_allowed_test_domain()
+        cls._remove_allowed_test_domain()
         cursor = connection.cursor()
         cursor.execute('DROP SCHEMA IF EXISTS test CASCADE')
 
@@ -50,7 +50,7 @@ class FastTenantTestCase(TenantTestCase):
     @classmethod
     def setUpClass(cls):
         cls.sync_shared()
-        cls.add_allowed_test_domain()
+        cls._add_allowed_test_domain()
         tenant_domain = 'tenant.test.com'
 
         TenantModel = get_tenant_model()
@@ -65,4 +65,4 @@ class FastTenantTestCase(TenantTestCase):
     @classmethod
     def tearDownClass(cls):
         connection.set_schema_to_public()
-        cls.remove_allowed_test_domain()
+        cls._remove_allowed_test_domain()
